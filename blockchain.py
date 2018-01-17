f04b0e63fbb07786037d57f87b88690ca121b2d6
SsangWoo <tkddn204@gmail.com>
2018-01-17 14:32:07 +0900

    한글주석

--- blockchain.py
+++ blockchain.py
@@ -19,9 +19,9 @@ class Blockchain:
 
     def register_node(self, address):
         """
-        Add a new node to the list of nodes
+        노드의 리스트에 새로운 노드를 추가합니다.
 
-        :param address: Address of node. Eg. 'http://192.168.0.5:5000'
+        :param address: 노드의 주소입니다. (예: http://192.168.0.5:5000)
         """
 
         parsed_url = urlparse(address)
@@ -29,10 +29,10 @@ class Blockchain:
 
     def valid_chain(self, chain):
         """
-        Determine if a given blockchain is valid
+        주어진 블록체인이 유효한지 체크합니다.
 
-        :param chain: A blockchain
-        :return: True if valid, False if not
+        :param chain: 블록체인
+        :return: 유효하면 True, 아니면 False
         """
 
         last_block = chain[0]
@@ -58,19 +58,19 @@ class Blockchain:
 
     def resolve_conflicts(self):
         """
-        This is our consensus algorithm, it resolves conflicts
-        by replacing our chain with the longest one in the network.
+        이것은 우리의 '합의 알고리즘' 입니다.
+        네트워크에서 가장 긴 체인으로 교체함으로써 충돌을 해결합니다.
 
-        :return: True if our chain was replaced, False if not
+        :return: 재배치가 되면 True, 되지 않았으면 False
         """
 
         neighbours = self.nodes
         new_chain = None
 
-        # We're only looking for chains longer than ours
+        # 우리는 우리의 체인보다 긴 체인들을 찾아야 합니다.
         max_length = len(self.chain)
 
-        # Grab and verify the chains from all the nodes in our network
+        # 네트워크에서 모든 노드들로부터 체인들을 잡고 검증해야 합니다.
         for node in neighbours:
             response = requests.get(f'http://{node}/chain')
 
@@ -78,12 +78,12 @@ class Blockchain:
                 length = response.json()['length']
                 chain = response.json()['chain']
 
-                # Check if the length is longer and the chain is valid
+                # 길이가 더 긴지 아닌지, 체인이 유효한지를 체크합니다.
                 if length > max_length and self.valid_chain(chain):
                     max_length = length
                     new_chain = chain
 
-        # Replace our chain if we discovered a new, valid chain longer than ours
+        # 새로운 우리 것보다 더 길고 유효한 체인을 발견하면 체인을 재배치합니다.
         if new_chain:
             self.chain = new_chain
             return True
@@ -92,11 +92,11 @@ class Blockchain:
 
     def new_block(self, proof, previous_hash):
         """
-        Create a new Block in the Blockchain
+        블록체인에서 새로운 블록을 만듭니다.
 
-        :param proof: The proof given by the Proof of Work algorithm
-        :param previous_hash: Hash of previous Block
-        :return: New Block
+        :param proof: '작업증명 알고리즘'에 의해 주어지는 증명
+        :param previous_hash: 이전 블록의 해쉬값
+        :return: 새로운 블록을 반환합니다.
         """
 
         block = {
@@ -107,7 +107,7 @@ class Blockchain:
             'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
 
-        # Reset the current list of transactions
+        # 트랜잭션들의 현재 리스트를 리셋합니다.
         self.current_transactions = []
 
         self.chain.append(block)
@@ -115,12 +115,12 @@ class Blockchain:
 
     def new_transaction(self, sender, recipient, amount):
         """
-        Creates a new transaction to go into the next mined Block
+        다음 채굴된 블록으로 들어가는 새로운 트랜잭션을 만듭니다.
 
-        :param sender: Address of the Sender
-        :param recipient: Address of the Recipient
-        :param amount: Amount
-        :return: The index of the Block that will hold this transaction
+        :param sender: 송신자의 주소
+        :param recipient: 수령인의 주소
+        :param amount: 금액
+        :return: 이 트랜잭션을 '보류할' 블록의 인덱스를 반환합니다.
         """
         self.current_transactions.append({
             'sender': sender,
@@ -137,20 +137,21 @@ class Blockchain:
     @staticmethod
     def hash(block):
         """
-        Creates a SHA-256 hash of a Block
+        블록의 SHA-256 해쉬값을 만듭니다.
 
-        :param block: Block
+        :param block: 블록
         """
 
-        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        # 우리는 JSON이 '순서대로인지'를 확실히 해야만 합니다.
+        # 확실히 하지 않으면 일치하지 않는 해쉬가 생길 겁니다.
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
     def proof_of_work(self, last_proof):
         """
-        Simple Proof of Work Algorithm:
-         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
-         - p is the previous proof, and p' is the new proof
+        간단한 작업증명 알고리즘:
+         - 해쉬(pp')가 4개의 0을 포함하도록 하는 숫자 p'를 찾습니다. 여기서 p는 이전의 p'입니다.
+         - p는 이전의 '증명'이며, p'는 새로운 '증명'입니다.
         """
 
         proof = 0
@@ -162,11 +163,11 @@ class Blockchain:
     @staticmethod
     def valid_proof(last_proof, proof):
         """
-        Validates the Proof
+        증명의 유효성을 판단합니다.
 
-        :param last_proof: Previous Proof
-        :param proof: Current Proof
-        :return: True if correct, False if not.
+        :param last_proof: 이전 증명
+        :param proof: 현재 증명
+        :return: 맞으면 True, 틀리면 False
         """
 
         guess = f'{last_proof}{proof}'.encode()
@@ -174,32 +175,32 @@ class Blockchain:
         return guess_hash[:4] == "0000"
 
 
-# Instantiate the Node
+# 노드 객체화(플라스크 객체)
 app = Flask(__name__)
 
-# Generate a globally unique address for this node
+# 이 노드에서 전역으로 유일한 주소값을 생성합니다.
 node_identifier = str(uuid4()).replace('-', '')
 
-# Instantiate the Blockchain
+# 블록체인 객체화
 blockchain = Blockchain()
 
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    # We run the proof of work algorithm to get the next proof...
+    # 우리는 다음 증명을 얻기 위해서 작업 증명 알고리즘을 돌려야합니다.
     last_block = blockchain.last_block
     last_proof = last_block['proof']
     proof = blockchain.proof_of_work(last_proof)
 
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mined a new coin.
+    # 우리는 증명을 찾아내는 보상을 받아야 합니다.
+    # 송신자는 이 노드가 새로운 코인을 채굴했음을 알리는 "0" 입니다.
     blockchain.new_transaction(
         sender="0",
         recipient=node_identifier,
         amount=1,
     )
 
-    # Forge the new Block by adding it to the chain
+    # 새로운 블록을 체인에 추가하여 구축합니다.
     previous_hash = blockchain.hash(last_block)
     block = blockchain.new_block(proof, previous_hash)
 
@@ -217,15 +218,15 @@ def mine():
 def new_transaction():
     values = request.get_json()
 
-    # Check that the required fields are in the POST'ed data
+    # 요구하는 필드들이 POST 데이터에 있는지 체크합니다.
     required = ['sender', 'recipient', 'amount']
     if not all(k in values for k in required):
-        return 'Missing values', 400
+        return '값이 존재하지 않습니다.', 400
 
-    # Create a new Transaction
+    # 새로운 트랜잭션을 만듭니다.
     index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
 
-    response = {'message': f'Transaction will be added to Block {index}'}
+    response = {'message': f'트랜잭션이 {index}번 블록에 추가될 것입니다.'}
     return jsonify(response), 201
 
 
@@ -244,13 +245,13 @@ def register_nodes():
 
     nodes = values.get('nodes')
     if nodes is None:
-        return "Error: Please supply a valid list of nodes", 400
+        return "에러 : 노드들의 유효한 리스트를 공급해주세요.", 400
 
     for node in nodes:
         blockchain.register_node(node)
 
     response = {
-        'message': 'New nodes have been added',
+        'message': '새로운 노드들이 추가되었습니다.',
         'total_nodes': list(blockchain.nodes),
     }
     return jsonify(response), 201
@@ -262,12 +263,12 @@ def consensus():
 
     if replaced:
         response = {
-            'message': 'Our chain was replaced',
+            'message': '블록체인이 재배치되었습니다.',
             'new_chain': blockchain.chain
         }
     else:
         response = {
-            'message': 'Our chain is authoritative',
+            'message': '우리 블록체인은 강한 권한을 가지고 있습니다.',
             'chain': blockchain.chain
         }
 
